ce5967e0b2e43a9788db9f32e71cdbf418fe6580
FadhilahBening12 <fadhilah_cahyani_33rpl@student.smktelkom-mlg.sch.id>
2025-11-24 16:47:22 +0700

    Add files via upload

---
+++ data/berita.ts
@@ -0,0 +1,20 @@
+export const beritaList = [
+  {
+    id: "1",
+    nama: "Teknik Dasar Renang Gaya Bebas",
+    tanggal: "12 Januari 2025",
+    isi: "Renang gaya bebas merupakan salah satu teknik dasar yang harus dikuasai atlet pemula..."
+  },
+  {
+    id: "2",
+    nama: "Persiapan Atlet Untuk Kejuaraan Nasional",
+    tanggal: "18 Januari 2025",
+    isi: "Kejuaraan nasional membutuhkan persiapan fisik dan mental yang matang. Pelatih harus..."
+  },
+  {
+    id: "3",
+    nama: "Tips Berlatih Renang Untuk Pemula",
+    tanggal: "25 Januari 2025",
+    isi: "Bagi pemula, renang bisa terasa sulit. Namun dengan latihan rutin dan teknik yang benar..."
+  },
+];
